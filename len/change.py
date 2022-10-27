3ff80e858ca8cdcb5d1f0c51351c384afff14d7b
[Kite7928] <[2250380443@qq.com]>
2022-10-27 21:26:40 +0800

    write py  the frist

---
+++ len/change.py
@@ -0,0 +1,25 @@
+# # 默认是字符串输出
+# a = input("请输入第一个数字")
+# b = input("请输入第2个数字")
+#
+# a = int(a)
+# b = int(b)
+# print("计算结果为:", a + b )
+
+# break让当前这个循环立即停止
+# continue:停止当前本次循环，继续执行下一次循环
+# while 循环 break continue
+# while  True:
+#     content = input("请输入你要喷的内容(q结束):")
+#     if content == "q":
+#         break
+#         print("发送给下路的:",content)
+
+# 从1-10
+i = 1
+while i <= 10:
+    if i == 4:
+        i = i + 1
+        continue  # 终止本次继续执行下一次循环
+    print(i)
+    i = i + 1
